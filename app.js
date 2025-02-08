7642bfbca896d8c3178b9ecd54ba0ad90cde4665
Abdul Rahman <abdulrahman161004@gmail.com>
2025-02-08 22:08:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const subscribeBtn = document.getElementById("subscribe_Btn");
+const article_btn = document.getElementById("article-btn");
+
+subscribeBtn.addEventListener("click", () => {
+    alert("SUBSCRIBED!");
+});
+
+article_btn.addEventListener("click", () => {
+    alert("Saved Article!");
+});
